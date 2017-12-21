a2891a0940ca18d52d473b0f7106328e25c4a1ea
niema ahmed <niemawidaha@ac.c4q.nyc>
2017-12-20 21:40:57 -0500

    complete part 03

--- app/src/main/java/c4q/nyc/unit4/BottomFragment.java
+++ app/src/main/java/c4q/nyc/unit4/BottomFragment.java
@@ -1,8 +1,11 @@
 package c4q.nyc.unit4;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -142,6 +145,27 @@ public class BottomFragment extends Fragment {
 
     }
 
+    public class BottomFragmentRecyclerView extends Activity {
+        private List<Book> bookList = new ArrayList<>();
+        private RecyclerView recyclerView;
+        private BookAdapter bookAdapter;
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_bottom);
+
+            recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+            bookAdapter = new BookAdapter(bookList);
+
+            RecyclerView.LayoutManager bookLayoutManager = new LinearLayoutManager(getApplicationContext());
+            recyclerView.setLayoutManager(bookLayoutManager);
+
+
+
+        }
+    }
 
 
 }
